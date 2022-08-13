435cb443b92251f82ebcb70b23cd142a0267d98c
GitPro10 <israfilmiya120@gmail.com>
2022-08-13 23:27:14 +0600

    Multiple FTP Server linking support

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 import FtpFiles from './getfiles.js'
+import ftpServers from "./serversList.js"
 import nodemailer from "nodemailer"
 import * as dotenv from 'dotenv'
 dotenv.config()
 
+
 let myEmail = process.env.EMAIL
 let googleEmailPass = process.env.PASS
-
 let personEmail = process.env.SENDMAILTO
 
 
 
 async function sendMailTo(email, htmlBody) {
-  let testAccount = await nodemailer.createTestAccount();
 
   let transporter = nodemailer.createTransport({
     service: 'gmail',
@@ -23,7 +23,7 @@ async function sendMailTo(email, htmlBody) {
   });
 
   let info = await transporter.sendMail({
-    from: '"'+process.env.SHOWNAME+'" <no-reply@ftp.com>',
+    from: '"' + process.env.SHOWNAME + '" <no-reply@ftp.com>',
     to: email,
     subject: "Update",
     html: htmlBody
@@ -32,81 +32,60 @@ async function sendMailTo(email, htmlBody) {
 }
 
 
-function credentialsDefiner(){
-  
-  let credentials = {
-    connTimeout: 300000,
-    pasvTimeout: 300000,
+ftpServers.map((serverInfo, index) => {
+
+
+
+  let ftpCredentials = serverInfo.info
+
+  let prevArray = []
+
+  const studioClickHouseFTPwatcher = new FtpFiles({
+    ftpCredentials,
+    cron: process.env.INTERVALCRON
+    // fileExtension: '.zip', // optional
+    // fileNameContains: 'GB' // optional
+  })
+
+
+  Array.prototype.diff = (arr2) => {
+    return this.filter(x => !arr2.includes(x));
   }
-  
-  let host = process.env.HOST
-  let port = process.env.FTPPORT
-  let user = process.env.USERNAME
-  let password = process.env.PASSWORD
-  let anonymous = process.env.ANONYMOUS
-  
-  credentials.host = host
-  if(port) credentials.port = port
-  if (!anonymous) {
-    credentials.user = user
-    credentials.password = password
+
+  const handleSnapshot = async (snapshot) => {
+
+    if (snapshot != prevArray) {
+      if (prevArray.length) {
+        if (prevArray.length > snapshot.length) {
+          console.log("FILE/s DELETED !!")
+
+          await sendMailTo(personEmail, `<h3>SOME FILE/s MAY GOT DELETED, CHECK THE FTP SERVER OF ${serverInfo.additionalInfo.name} </h3>`)
+        }
+        if (prevArray.length < snapshot.length) {
+          console.log("FILE/s ADDED !!")
+
+          await sendMailTo(personEmail, `<h3>GOT NEW FILE/s, CHECK THE FTP SERVER OF ${serverInfo.additionalInfo.name} </h3>`)
+
+        }
+      } else {
+        console.log("NO CHANGES DETECTED, TILL NOW !! (PROBABLY BECAUSE OF FIRST INITIALIZATION)", serverInfo.additionalInfo.name)
+      }
+      prevArray = snapshot
+    } else prevArray = snapshot
+    console.log(prevArray)
+
+    //studioClickHouseFTPwatcher.stop()
   }
-  
-  return credentials
-  
-}
 
-const ftpCredentials = credentialsDefiner()
+  const handleError = (error) => {
+    console.error(error)
+  }
 
-let prevArray = []
+  studioClickHouseFTPwatcher.on('error', handleError)
+  studioClickHouseFTPwatcher.on('snapshot', handleSnapshot)
 
-const speedtestWatcher = new FtpFiles({
-  ftpCredentials: ftpCredentials,
-  cron: process.env.INTERVALCRON
-  // fileExtension: '.zip', // optional
-  // fileNameContains: 'GB' // optional
+  studioClickHouseFTPwatcher.watch()
+
 })
-
-Array.prototype.diff = (arr2) => {
-  return this.filter(x => !arr2.includes(x));
-}
-
-const handleSnapshot = async (snapshot) => {
-
-  if (snapshot != prevArray) {
-    if (prevArray.length) {
-      if (prevArray.length > snapshot.length) {
-        console.log("\n")
-        console.log("FILE/s DELETED !!")
-        await sendMailTo(personEmail, "<h3>SOME FILE/s MAY GOT DELETED, CHECK THE FTP SERVER</h3>")
-      }
-      if (prevArray.length < snapshot.length) {
-        console.log("\n")
-        console.log("FILE/s ADDED !!")
-
-        await sendMailTo(personEmail, "<h3>NEW FILE/s GOT ADDED, CHECK THE FTP SERVER</h3>")
-
-      }
-    } else {
-      console.log("\n")
-      console.log("NO CHANGES DETECTED, TILL NOW !! (PROBABLY BECAUSE OF FIRST INITIALIZATION)")
-    }
-    prevArray = snapshot
-  } else prevArray = snapshot
-  console.log("\n")
-  console.log(prevArray)
-  console.log("\n")
-
-  //speedtestWatcher.stop()
-}
-
-const handleError = (error) => {
-  console.error(error)
-}
-
-speedtestWatcher.on('error', handleError)
-speedtestWatcher.on('snapshot', handleSnapshot)
-
-speedtestWatcher.watch()
 
 //
---
+++ serversList.js
@@ -0,0 +1,48 @@
+let ftpServers = 
+
+[
+  {
+    info: {
+      host: '92.205.7.115',
+      user: 'see360degrees@studioclickhouse.com',
+      password: '360degrees@2022'
+    },
+    additionalInfo: {
+      name: 'See 360 Degrees'
+    }
+  },
+  {
+    info: {
+      host: '92.205.7.115',
+      user: 'jrphotography@studioclickhouse.com',
+      password: 'jrphotography@2022'
+    },
+    additionalInfo: {
+      name: 'JR Photography'
+    }
+  },
+  {
+    info: {
+      host: '92.205.7.115',
+      user: 'xostudio@studioclickhouse.com',
+      password: 'xostudio@2022'
+    },
+    additionalInfo: {
+      name: 'XO Studio'
+    }
+  },
+  {
+    info: {
+      host: '92.205.7.115',
+      user: 'hhstudio@studioclickhouse.com',
+      password: 'hhstudio@2022'
+    },
+    additionalInfo: {
+      name: 'HH Studio'
+    }
+  },
+]
+
+
+
+export default ftpServers
